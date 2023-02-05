18f5612240938ea078a4d7bcb36154ad00746e39
CodeChild010 <CodeChild010@gmail.com>
2023-02-05 17:23:25 -0500

    Playing a round! (Get it?)
    - new function playround() takes the return value of getComputerChoice and a predefined value playerSelection to play one round of the game.
    - When getComputerChoice() returns Paper and the predefined value of playerSelection equals rock will run through the playround() function return "You lose! Paper beats Rock".

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 
-alert(getComputerChoice());
 
-function getComputerChoice() {
+
+
+const getComputerChoice = (function() {
     result = Math.floor(Math.random() * 3);
     if (result === 0) {
         result = "Rock";
@@ -13,4 +14,21 @@ function getComputerChoice() {
         result = "Scissors";
     }
     return result;
-};
+});
+
+
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+    playerSelection = playerSelection.toUpperCase();
+    
+    if (computerSelection === "Paper" && playerSelection === "ROCK") {
+        result = alert("You lose! Paper beats Rock")
+    }
+    return result;
+}
+
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log((playRound(playerSelection, computerSelection)));
