f9380d351fd6aec3da8ee3e31b02f7348f92ac16
CodeChild010 <CodeChild010@gmail.com>
2023-02-06 22:35:55 -0500

    Add all move possibilities & display round outcome
    - Revised playRound() to alert whether round winner, loser, or draw.

--- javascript.js
+++ javascript.js
@@ -18,9 +18,48 @@ const getComputerChoice = (function() {
 function playRound(playerSelection, computerSelection) {
     let result;
     
-    if (computerSelection === "Paper" && playerSelection === "ROCK") {
-        result = alert("You lose! Paper beats Rock")
-    }
+    if (computerSelection === "Paper") {
+
+        if (playerSelection === "ROCK") {          
+            result = alert("You lose! Paper beats Rock")
+
+            } else if (playerSelection === "SCISSOR") {
+            result = alert("You Win! Scissor beats paper")
+
+            } else if (playerSelection === "PAPER") {
+            result = alert("Draw. Paper = Paper")
+            }
+        }
+
+
+    else if (computerSelection === "Rock") {
+
+        if (playerSelection === "ROCK") {          
+            result = alert("Draw. Rock = Rock")
+
+            } else if (playerSelection === "SCISSOR") {
+            result = alert("You lose! Rock beats Scissor")
+
+            } else if (playerSelection === "PAPER") {
+            result = alert("You win! Paper beats Rock")
+            }   
+        }
+            
+            
+    else if (computerSelection === "Scissor") {
+
+        if (playerSelection === "ROCK") {          
+            result = alert("You win! Rock beats Scissor")
+
+            } else if (playerSelection === "SCISSOR") {
+            result = alert("Draw. Scissor = Scissor")
+
+            } else if (playerSelection === "PAPER") {
+            result = alert("You lose! Scissor beats Paper")
+            }
+        }
+                
+                
     return result;
 }
 
@@ -32,6 +71,7 @@ function game() {
         var playerSelection = prompt("What's your move, player?").toUpperCase();
         const computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
+    
     }
 }
 
